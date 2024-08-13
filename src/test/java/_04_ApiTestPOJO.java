a37165303d6d2c07a5794a6831e757ee8cc206e3
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-14 01:00:57 +0200

    update

--- src/test/java/_04_ApiTestPOJO.java
+++ src/test/java/_04_ApiTestPOJO.java
@@ -1,5 +1,6 @@
 import models.Location;
 import models.Place;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 
@@ -12,10 +13,10 @@ public class _04_ApiTestPOJO {
                 given()
 
                         .when()
-                        .get("http://api.zippopotam.us/tr/01000")
+                        .get("http://api.zippopotam.us/us/90210")
 
                         .then()
-                        .log().body()
+//                        .log().body()
                         .extract().body().as(Location.class)
                                             // as type of location
                 ;
@@ -26,6 +27,32 @@ public class _04_ApiTestPOJO {
         for(Place p : locationObj.getPlaces()){
             System.out.println("p = " + p);
         }
+
+    }
+
+    // From the data returned by the http://api.zippopotam.us/tr/01000 endpoint,
+    // print out the information related to 'Dörtağaç Köyü'.
+
+    @Test
+    public void t2extractJsonAll_POJO() {
+
+        Location locationObj =
+                given()
+
+                        .when()
+                        .get("http://api.zippopotam.us/tr/01000")
+
+                        .then()
+                        .extract().body().as(Location.class)
+                ;
+
+        for (Place p : locationObj.getPlaces()) {
+            if (p.getPlaceName().contains("Dörtağaç Köyü")) {
+                                //or you can use .equalsIgnoreCase
+                System.out.println(p);
+            }
+        }
+
 
     }
 }

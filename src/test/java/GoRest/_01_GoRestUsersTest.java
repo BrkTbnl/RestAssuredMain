95244082463d4546007641d588b93a6a3e9f3394
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-15 17:03:50 +0200

    Faker added to test case

--- src/test/java/GoRest/_01_GoRestUsersTest.java
+++ src/test/java/GoRest/_01_GoRestUsersTest.java
@@ -1,5 +1,6 @@
 package GoRest;
 
+import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
@@ -13,16 +14,21 @@ public class _01_GoRestUsersTest {
     "gender":"male",
     "email":"{{$randomEmail}}",
     "status":"active"}
-
-
     */
+
+    Faker randomFaker = new Faker();
+    int userId = 0;
 
     @Test
     public void createUser() {
-        int userId =
+
+        String fullName = randomFaker.name().fullName();
+        String email = randomFaker.internet().emailAddress();
+
+        userId =
         given() // body, token, contentType
                 .header("Authorization", "Bearer 6e2a70107d231adae49cbeb2d30685ac536fef76a7401004e39ac3f5a3b2088a")
-                .body("{\"name\":\"bt1\", \"gender\":\"male\", \"email\":\"nomail1@allowed.here\",  \"status\":\"active\"}")
+                .body("{\"name\":\""+fullName+"\", \"gender\":\"male\", \"email\":\""+email+"\",  \"status\":\"active\"}")
                 .contentType(ContentType.JSON)
 
                 .when()

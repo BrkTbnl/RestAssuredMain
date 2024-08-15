401e568e170d506ab7909a75c0a35591b21c1ffe
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-15 16:34:40 +0200

    additional test case and todo

--- src/test/java/_06_PathAndJSONPath.java
+++ src/test/java/_06_PathAndJSONPath.java
@@ -1,4 +1,10 @@
+import io.restassured.response.Response;
+import models.Place;
+import models.User;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 
 public class _06_PathAndJSONPath {
@@ -39,9 +45,50 @@ public class _06_PathAndJSONPath {
         System.out.println("postCode = " + postCode);
     }
 
+    @Test
+    public void getZipCode() {
+        Response response =
+                given()
+
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        .log().body()
+                        .extract().response();
+
+//        Location locationPathAs = response.as(Location.class);
+//          instead of doin this
+
+        List<Place> places = response.jsonPath().getList("places", Place.class);
+        System.out.println("places = " + places);
+
+    }
+
     /*
      Print only the "data" part returned from the https://gorest.co.in/public/v1/users endpoint using POJO conversion.
     */
+
+    @Test
+    public void getUserData() {
+
+        List<User> userData =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+
+                        .then()
+//              .log().body()
+                        .extract().jsonPath().getList("data", User.class);
+
+        //System.out.println("userData.get(0).getEmail() = " + userData.get(0).getEmail());
+
+        for (User user : userData) {
+            System.out.println("user = " + user);
+        }
+    }
 
 
 

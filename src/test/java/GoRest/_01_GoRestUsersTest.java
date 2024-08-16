bc33a9123e7ed6e2c07ab867c4e5c16688804e7a
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-16 14:59:13 +0200

    get user by id test case added

--- src/test/java/GoRest/_01_GoRestUsersTest.java
+++ src/test/java/GoRest/_01_GoRestUsersTest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class _01_GoRestUsersTest {
     /*
@@ -23,7 +24,7 @@ public class _01_GoRestUsersTest {
     Faker randomFaker = new Faker();
     int userId = 0;
 
-    @Test
+    @Test(enabled = false)
     public void createUserJson() {
 
         String fullName = randomFaker.name().fullName();
@@ -75,7 +76,7 @@ public class _01_GoRestUsersTest {
         System.out.println("userId = " + userId);
     }
 
-    @Test
+    @Test(enabled = false)
     public void createUserClass() {
 
         String fullName = randomFaker.name().fullName();
@@ -103,6 +104,23 @@ public class _01_GoRestUsersTest {
                         .extract().path("id");
 
         System.out.println("userId = " + userId);
+    }
+
+    @Test(dependsOnMethods = "createUserMap")
+    public void getUserById(){
+
+        given()
+                .header("Authorization", "Bearer 6e2a70107d231adae49cbeb2d30685ac536fef76a7401004e39ac3f5a3b2088a")
+
+                .when()
+                .get("https://gorest.co.in/public/v2/users/" +userId)
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(userId))
+
+        ;
     }
 
 
